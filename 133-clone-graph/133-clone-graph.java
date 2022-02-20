86ad2875276dc118fac076a2b7407882e8a64e5e
Huu Khang Nguyen <52158153+huukhang1512@users.noreply.github.com>
2022-02-20 21:04:13 +1100

    Time: 39 ms (53.80%), Space: 43.2 MB (24.90%) - LeetHub

--- 133-clone-graph/133-clone-graph.java
+++ 133-clone-graph/133-clone-graph.java
@@ -20,20 +20,21 @@ class Node {
 
 class Solution {
     public Node cloneGraph(Node node) {
-        HashMap<Integer,Node> map = new HashMap();
-        return clone(node, map);
+        HashMap<Integer,Node> map = new HashMap<>();
+        return clone(node,map);
     }
-    public Node clone(Node node, HashMap<Integer,Node> map){
+    
+    public Node clone (Node node, HashMap<Integer,Node> map) {
         if(node == null){
             return null;
         }
-        if(map.get(node.val) != null){
+        if(map.containsKey(node.val)){
             return map.get(node.val);
         }
-        Node newNode = new Node(node.val, new ArrayList<Node>());
-        map.put(newNode.val, newNode);
+        Node newNode = new Node(node.val, new ArrayList<>());
+        map.put(newNode.val,newNode);
         for(Node neighbor: node.neighbors){
-            newNode.neighbors.add(clone(neighbor,map));
+            newNode.neighbors.add(clone(neighbor, map));
         }
         return newNode;
     }
