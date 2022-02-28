d230341e2cdd700fde70ad2baf4783407b743350
Huu Khang Nguyen <52158153+huukhang1512@users.noreply.github.com>
2022-02-28 17:21:35 +1100

    Time: 44 ms (8.32%), Space: 75.7 MB (5.00%) - LeetHub

--- 207-course-schedule/207-course-schedule.java
+++ 207-course-schedule/207-course-schedule.java
@@ -3,26 +3,31 @@ class Solution {
         int[][] matrix = new int[numCourses][numCourses]; // i -> j
         int[] indegree = new int[numCourses];
 
+        // topological sorting, kahn algo
         for (int i=0; i<prerequisites.length; i++) {
-            int ready = prerequisites[i][0];
+            int subject = prerequisites[i][0];
             int pre = prerequisites[i][1];
-            if (matrix[pre][ready] == 0)
-                indegree[ready]++; //duplicate case
-            matrix[pre][ready] = 1;
+            
+            if (matrix[pre][subject] == 0){
+                indegree[subject]++; 
+            }
+            matrix[pre][subject] = 1;
         }
 
         int count = 0;
         Queue<Integer> queue = new LinkedList();
         for (int i=0; i<indegree.length; i++) {
-            if (indegree[i] == 0) queue.offer(i);
+            if (indegree[i] == 0) queue.add(i);
         }
         while (!queue.isEmpty()) {
             int course = queue.poll();
             count++;
             for (int i=0; i<numCourses; i++) {
                 if (matrix[course][i] != 0) {
-                    if (--indegree[i] == 0)
-                        queue.offer(i);
+                    indegree[i]--;
+                    if (indegree[i] == 0){
+                        queue.add(i);
+                    }
                 }
             }
         }
