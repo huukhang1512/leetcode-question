4d6760601270f1721daeaba22c802665ed4ce9a7
Huu Khang Nguyen <52158153+huukhang1512@users.noreply.github.com>
2022-02-28 23:58:37 +1100

    Time: 4 ms (99.98%), Space: 224.8 MB (24.71%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
@@ -13,21 +13,33 @@ class Solution {
         if(head == null || head.next == null){
             return null;
         }
-        int count = 0;
-        ListNode itr = head;
-        while(itr != null){
-            count++;
-            itr = itr.next;
-        }
-        itr = head;
-        int mid = (int) Math.floor(count/2);
+//         int count = 0;
+//         ListNode itr = head;
+//         while(itr != null){
+//             count++;
+//             itr = itr.next;
+//         }
+//         itr = head;
+//         int mid = (int) Math.floor(count/2);
         
-        while(mid > 1){
-            itr = itr.next;
-            mid--;
+//         while(mid > 1){
+//             itr = itr.next;
+//             mid--;
+//         }
+        
+//         itr.next = itr.next.next;
+//         return head;
+        
+        //1 pass floyd and tortoise
+        ListNode slow = head;
+        ListNode fast = head.next.next;
+        
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
         }
         
-        itr.next = itr.next.next;
+        slow.next = slow.next.next;
         return head;
     }
 }
