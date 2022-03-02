4085479481e869b2928d6c003212bf3507a6e6b0
Huu Khang Nguyen <52158153+huukhang1512@users.noreply.github.com>
2022-03-02 22:15:22 +1100

    Time: 0 ms (100.00%), Space: 42.5 MB (26.09%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.java
@@ -4,11 +4,45 @@ class Solution {
         // find the part where the array rotate,
         // for O(n), just simply use Math.min.
         
-        
-        int min = Integer.MAX_VALUE;
+        // O(n)        
+        /*int min = Integer.MAX_VALUE;
         for(int i = 0; i < nums.length; i++){
             min = Math.min(nums[i],min);
         }
-        return min;
+        return min;*/
+        
+        // What I know
+        /*
+        i'm pretty sure the first item would always more than the last Item
+        
+        */
+        
+        if(nums[0] < nums[nums.length -1]){
+            return nums[0];
+        }
+        if(nums.length == 1){
+            return nums[0];
+        }
+                
+        int left = 0;
+        int right = nums.length - 1;
+        while(right >= left){
+            int mid = left + (right - left) / 2;
+            
+            if(nums[mid] > nums[mid + 1]){
+                return nums[mid+1];
+            }
+            if (nums[mid - 1] > nums[mid]){
+                return nums[mid];
+            }
+            
+            if(nums[mid] > nums[0]){
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+        
     }
 }
