97ffe7de9b676295cc19a6514e88febecdf4a114
Huu Khang Nguyen <52158153+huukhang1512@users.noreply.github.com>
2022-03-05 00:58:19 +1100

    Time: 3 ms (46.12%), Space: 48.1 MB (13.90%) - LeetHub

--- 57-insert-interval/57-insert-interval.java
+++ 57-insert-interval/57-insert-interval.java
@@ -1,23 +1,20 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        // the goal is to continously add new interval and merge the interval together
+        // the goal is keeping track of the minimum start, and the maximum end of the next interval to be added
         ArrayList<int[]> result = new ArrayList();
         for(int[] interval : intervals){
-            // if the current interval[0] (start) is more than the new interval, 
-            // it means that the new interval should be before the current interval, set the new interval into interval
-            // as it will be use next as the larger interval
+            // if the current interval[0] (start) is more than the new interval, -> the entire interval is bigger
             if(interval[0] > newInterval[1]){
                result.add(newInterval);
                newInterval = interval;
             }
             
-            // quite obviously here, the interval is before the new interval
+            // quite obviously here, the interval is smaller than the new interval
             else if(interval[1] < newInterval[0]){
                 result.add(interval);
             } 
             
-            // else, keeping the min value between the start of both newInterval and the current Interval, 
-            // as well as the max of the end value of the interval
+            // else, init a new interval with the min between the start value, and the max of the end value
             else {
                 newInterval[0] = Math.min(newInterval[0],interval[0]); 
                 newInterval[1] = Math.max(newInterval[1],interval[1]); 
