5b02cff08f8a4a80dae4c534ad0b2b89e17cd230
Huu Khang Nguyen <52158153+huukhang1512@users.noreply.github.com>
2022-03-05 00:23:21 +1100

    Time: 2 ms (77.47%), Space: 47.8 MB (22.69%) - LeetHub

--- 57-insert-interval/57-insert-interval.java
+++ 57-insert-interval/57-insert-interval.java
@@ -3,8 +3,8 @@ class Solution {
         ArrayList<int[]> result = new ArrayList();
         for(int[] interval : intervals){
             if(interval[0] > newInterval[1]){
-               result.add(newInterval);
-               newInterval = interval;
+                result.add(newInterval);
+                newInterval = interval;
             }
             else if(interval[1] < newInterval[0]){
                 result.add(interval);
