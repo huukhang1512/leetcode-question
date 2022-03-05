54f9afa91144bdfcfdc3d323496b33a1ae8f25d1
Huu Khang Nguyen <52158153+huukhang1512@users.noreply.github.com>
2022-03-05 23:23:04 +1100

    Time: 3 ms (40.97%), Space: 44.8 MB (30.60%) - LeetHub

---
+++ 152-maximum-product-subarray/152-maximum-product-subarray.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int res = nums[0];
+        int min = nums[0];
+        int max = nums[0];
+        for(int i = 1;i< nums.length;i++){
+            int tempMax = max;
+        
+            // equivalent to Math.max/min between 3 var current nums, current max * num, and cur min * num
+            // Math.max(curNum, curMax * curNum, curMin * curNum)
+            max = Math.max(nums[i], Math.max(max*nums[i], min * nums[i]));
+            min = Math.min(nums[i], Math.min(tempMax*nums[i], min * nums[i]));   
+            res = Math.max(res,max);
+        }
+        return res;
+    }
+}
