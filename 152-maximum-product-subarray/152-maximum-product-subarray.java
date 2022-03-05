90e10158c19b419e938b3a5c23fb7d4d9dfdf312
Huu Khang Nguyen <52158153+huukhang1512@users.noreply.github.com>
2022-03-05 23:25:27 +1100

    Time: 2 ms (72.19%), Space: 44.7 MB (34.66%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.java
+++ 152-maximum-product-subarray/152-maximum-product-subarray.java
@@ -9,6 +9,8 @@ class Solution {
             // equivalent to Math.max/min between 3 var current nums, current max * num, and cur min * num
             // Math.max(curNum, curMax * curNum, curMin * curNum)
             max = Math.max(nums[i], Math.max(max*nums[i], min * nums[i]));
+            
+            // Math.min(curNum, curMax * curNum, curMin * curNum)
             min = Math.min(nums[i], Math.min(tempMax*nums[i], min * nums[i]));   
             res = Math.max(res,max);
         }
