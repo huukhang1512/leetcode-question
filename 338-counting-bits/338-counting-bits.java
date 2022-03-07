6007fbdb005d37ea75174819f3f49f9b9a0fc420
Huu Khang Nguyen <52158153+huukhang1512@users.noreply.github.com>
2022-03-07 16:13:13 +1100

    Time: 4 ms (34.78%), Space: 47.6 MB (68.51%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -1,5 +1,7 @@
 class Solution {
     public int[] countBits(int n) {
+        // O(nlogn)
+        /*
         int[] res = new int[n+1];
         for(int i = 0; i <= n; i++){
             int count = 0;
@@ -11,5 +13,27 @@ class Solution {
             res[i] = count;
         }
         return res;
+        */
+        
+        // O(n)
+        
+        if(n == 0){
+            return new int[]{0};
+        }
+        if(n == 1){
+            return new int[]{0,1};
+        }
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++){
+            if(i % 2 == 0){
+                dp[i] = dp[i/2];
+            } else {
+                dp[i] =  1 + dp[i / 2];
+            }
+        }
+        return dp;
+        
     }
 }
