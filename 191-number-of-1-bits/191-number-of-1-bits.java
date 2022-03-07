646e1c8989552a7f39e321319e34edc92ad4c368
Huu Khang Nguyen <52158153+huukhang1512@users.noreply.github.com>
2022-03-07 17:25:50 +1100

    Time: 1 ms (80.06%), Space: 41.2 MB (31.94%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.java
+++ 191-number-of-1-bits/191-number-of-1-bits.java
@@ -5,7 +5,7 @@ public class Solution {
         int count = 0;
         while(cur != 0){
             count += cur & 1;
-            cur = cur >>> 1;
+            cur = cur >>> 1; // unsigned shift bits on the right by 1
         }
         return count;
     }
