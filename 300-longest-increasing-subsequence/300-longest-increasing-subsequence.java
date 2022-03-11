3b07d7555b8a42501443e5763f25c2fea437afc9
Huu Khang Nguyen <52158153+huukhang1512@users.noreply.github.com>
2022-03-12 00:44:53 +1100

    Time: 56 ms (64.16%), Space: 41.4 MB (81.88%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,4 +1,6 @@
 class Solution {
+    // recursion + cache
+    /*
     int[] dp;
     public int lengthOfLIS(int[] nums) {
         dp = new int[nums.length + 1];
@@ -8,6 +10,7 @@ class Solution {
     public int dfs(int[] nums, int i, int prev_i){
         if(i >= nums.length) return 0;
         if(dp[prev_i + 1] != 0) return dp[prev_i + 1];
+
         int take = 0;
         int dontTake = dfs(nums, i+1, prev_i);
 
@@ -17,5 +20,23 @@ class Solution {
         dp[prev_i+1] = Math.max(take, dontTake);
         
         return dp[prev_i + 1];
+    }
+    */
+    
+    //bottomUp
+    
+    public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp,1); // for every element, there always exist one choice
+        int max = 0;
+        for(int i = nums.length - 1; i >= 0; i--){
+            for(int j = i+1; j < nums.length; j++){
+                if(nums[j] > nums[i]){
+                    dp[i] = Math.max(dp[i], 1 + dp[j]);
+                }
+            }
+            max = Math.max(dp[i],max);
+        }
+        return max;
     }
 }
