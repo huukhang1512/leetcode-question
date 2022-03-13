f6e487c0663b786b135c5e0a744e2bf8cf24feae
Huu Khang Nguyen <52158153+huukhang1512@users.noreply.github.com>
2022-03-14 10:51:57 +1100

    Time: 536 ms (12.73%), Space: 69.6 MB (7.53%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -30,3 +30,20 @@ class Solution {
         return false;
     }
 }
+
+/*
+greedy:
+
+class Solution {
+    public boolean canJump(int[] nums) {
+        int reachable = 0;
+        for(int i = 0; i < nums.length;i++){
+            if(reachable < i){
+                return false;
+            }
+            reachable= Math.max(reachable,i+ nums[i]);
+        }
+        return true;
+    }
+}
+*/
