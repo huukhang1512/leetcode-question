31ddba2beaff80142bbc3c7982849f3e65812df0
Huu Khang Nguyen <52158153+huukhang1512@users.noreply.github.com>
2022-03-13 15:13:41 +1100

    Time: 1 ms (100.00%), Space: 83.3 MB (100.00%) - LeetHub

---
+++ 2202-maximize-the-topmost-element-after-k-moves/2202-maximize-the-topmost-element-after-k-moves.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maximumTop(int[] nums, int k) {
+        if(nums.length == 1 && k % 2 == 1) {
+            return -1;
+        }
+        
+        
+        int toLook = k <= nums.length ? k - 1 : nums.length;
+        int maxSoFar = -1;
+        for(int i = 0; i < toLook; i++) {
+            maxSoFar = Math.max(nums[i], maxSoFar);
+        }
+        int lastElement = k < nums.length ? nums[k] : 0;
+        return Math.max(maxSoFar, lastElement);
+    }
+}
