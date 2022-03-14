1de8974b106e1ec3a92456b5fa0bc6d3bca8dc2d
Huu Khang Nguyen <52158153+huukhang1512@users.noreply.github.com>
2022-03-14 12:34:09 +1100

    Time: 4 ms (48.55%), Space: 68 MB (38.71%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,38 +1,38 @@
-class Solution {
-    public boolean canJump(int[] nums) {
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp, -1);
-        return dfs(nums,dp, 0);
-    }
+// class Solution {
+//     public boolean canJump(int[] nums) {
+//         int[] dp = new int[nums.length];
+//         Arrays.fill(dp, -1);
+//         return dfs(nums,dp, 0);
+//     }
     
-    public boolean dfs(int[] nums, int[] dp, int start){
-        if(start >= dp.length -1){
-            return true;
-        }
+//     public boolean dfs(int[] nums, int[] dp, int start){
+//         if(start >= dp.length -1){
+//             return true;
+//         }
         
-        if(nums[start] == 0){
-            dp[start] = 0;
-            return false;
-        }
+//         if(nums[start] == 0){
+//             dp[start] = 0;
+//             return false;
+//         }
         
-        if(dp[start] != -1){
-            return dp[start] == 1; // true
-        }
-        int numOfJumps = nums[start];
-        for(int i = 1; i <= numOfJumps;i++){
-            if(dfs(nums,dp, i+start)){
-                dp[i] = 1;
-                return true;   
-            }
-        }
+//         if(dp[start] != -1){
+//             return dp[start] == 1; // true
+//         }
+//         int numOfJumps = nums[start];
+//         for(int i = 1; i <= numOfJumps;i++){
+//             if(dfs(nums,dp, i+start)){
+//                 dp[i] = 1;
+//                 return true;   
+//             }
+//         }
         
-        dp[start] = 0;
-        return false;
-    }
-}
+//         dp[start] = 0;
+//         return false;
+//     }
+// }
 
-/*
-greedy:
+// /*
+// greedy:
 
 class Solution {
     public boolean canJump(int[] nums) {
@@ -46,4 +46,4 @@ class Solution {
         return true;
     }
 }
-*/
+// */
