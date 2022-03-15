981d5b2619cb3b012b3803e4639ca6a78547b0ac
Huu Khang Nguyen <52158153+huukhang1512@users.noreply.github.com>
2022-03-15 23:03:14 +1100

    Time: 14 ms (30.18%), Space: 45.9 MB (70.26%) - LeetHub

--- 207-course-schedule/207-course-schedule.java
+++ 207-course-schedule/207-course-schedule.java
@@ -1,3 +1,5 @@
+/*
+DFS 
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         if(numCourses <= 0) return false;
@@ -37,4 +39,47 @@ class Solution {
         }
         return true;
     }
+}
+*/
+// BFS
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        boolean[][] matrix = new boolean[numCourses][numCourses]; // i -> j
+        int[] indegree = new int[numCourses];
+
+        HashMap<Integer,Integer> map = new HashMap<Integer, Integer>();
+        
+        
+        // topological sorting, kahn algo
+        for (int i=0; i<prerequisites.length; i++) {
+            int subject = prerequisites[i][0];
+            int pre = prerequisites[i][1];
+            
+            if (!matrix[pre][subject]){
+                indegree[subject]++; 
+            }
+            matrix[pre][subject] = true;
+        }
+        
+        int count = 0;
+        Queue<Integer> queue = new LinkedList();
+        for (int i=0; i<indegree.length; i++) {
+            if (indegree[i] == 0) queue.add(i);
+        }
+        while (!queue.isEmpty()) {
+            int course = queue.poll();
+            count++;
+            for (int i=0; i<numCourses; i++) {
+                if (matrix[course][i]) { // check if there are any other subject that need course as req
+                    indegree[i]--;
+                    if (indegree[i] == 0){
+                        queue.add(i);
+                    }
+                }
+            }
+        }
+        return count == numCourses;
+    }
+
+    
 }
