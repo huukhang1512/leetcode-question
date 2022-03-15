5bb8c8e8b356752eabf467248b2da1bcb3cd59e1
Huu Khang Nguyen <52158153+huukhang1512@users.noreply.github.com>
2022-03-15 22:44:15 +1100

    Time: 70 ms (5.03%), Space: 55.2 MB (5.14%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        if(numCourses <= 0) return new int[0];
+        
+        HashMap<Integer, ArrayList<Integer>> preMap = new HashMap<>();
+        HashMap<Integer, Integer> inDegree = new HashMap<>();
+        
+        for(int i = 0 ; i < numCourses; i++){
+            preMap.put(i, new ArrayList<Integer>());
+            inDegree.put(i, 0);
+        }
+        
+        for(int[] prerequisite : prerequisites){
+            int subject = prerequisite[0];
+            int preSubject = prerequisite[1];
+            
+            preMap.get(preSubject).add(subject);
+            inDegree.put(subject, inDegree.get(subject) + 1);
+            
+        }
+        
+        int[] res = new int[numCourses];
+        int index = 0;
+        while(!inDegree.isEmpty()){
+            boolean isCyclic = false;
+            for(int course: inDegree.keySet()){
+                if(inDegree.get(course) == 0){ // no prerequisite, or 0 indegrees
+                    res[index++] = course;
+
+                    ArrayList<Integer> children = preMap.get(course);
+                    for(int child : children)
+                        inDegree.put(child, inDegree.get(child) - 1);
+                    inDegree.remove(course);
+                    isCyclic = true;
+                    break; 
+                }
+            }
+            if (!isCyclic)
+                return new int[0]; // empty array
+        }
+        return res;
+    }
+}
