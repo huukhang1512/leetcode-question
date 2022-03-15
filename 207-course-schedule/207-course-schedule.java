29c76beb6766b6ac3702b1d401aabb6dcd2106ea
Huu Khang Nguyen <52158153+huukhang1512@users.noreply.github.com>
2022-03-15 23:02:21 +1100

    Time: 33 ms (11.82%), Space: 42.7 MB (85.98%) - LeetHub

--- 207-course-schedule/207-course-schedule.java
+++ 207-course-schedule/207-course-schedule.java
@@ -1,41 +1,40 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        boolean[][] matrix = new boolean[numCourses][numCourses]; // i -> j
-        int[] indegree = new int[numCourses];
-
-        HashMap<Integer,Integer> map = new HashMap<Integer, Integer>();
+        if(numCourses <= 0) return false;
         
+        HashMap<Integer, ArrayList<Integer>> preMap = new HashMap<>();
+        HashMap<Integer, Integer> inDegree = new HashMap<>();
         
-        // topological sorting, kahn algo
-        for (int i=0; i<prerequisites.length; i++) {
-            int subject = prerequisites[i][0];
-            int pre = prerequisites[i][1];
+        for(int i = 0 ; i < numCourses; i++){
+            preMap.put(i, new ArrayList<Integer>());
+            inDegree.put(i, 0);
+        }
+        
+        for(int[] prerequisite : prerequisites){
+            int subject = prerequisite[0];
+            int preSubject = prerequisite[1];
             
-            if (!matrix[pre][subject]){
-                indegree[subject]++; 
-            }
-            matrix[pre][subject] = true;
+            preMap.get(preSubject).add(subject);
+            inDegree.put(subject, inDegree.get(subject) + 1);
+            
         }
         
-        int count = 0;
-        Queue<Integer> queue = new LinkedList();
-        for (int i=0; i<indegree.length; i++) {
-            if (indegree[i] == 0) queue.add(i);
-        }
-        while (!queue.isEmpty()) {
-            int course = queue.poll();
-            count++;
-            for (int i=0; i<numCourses; i++) {
-                if (matrix[course][i]) { // check if there are any other subject that need course as req
-                    indegree[i]--;
-                    if (indegree[i] == 0){
-                        queue.add(i);
-                    }
+        // the goal is to reduce the indegree one by one until no element
+        while(!inDegree.isEmpty()){
+            boolean isCyclic = true;
+            for(int course: inDegree.keySet()){
+                if(inDegree.get(course) == 0){ // no prerequisite, or 0 indegrees
+
+                    ArrayList<Integer> children = preMap.get(course);
+                    for(int child : children)
+                        inDegree.put(child, inDegree.get(child) - 1);
+                    inDegree.remove(course);
+                    isCyclic = false;
+                    break; 
                 }
             }
+            if (isCyclic) return false; // cycle detected, return empty array
         }
-        return count == numCourses;
+        return true;
     }
-
-    
 }
