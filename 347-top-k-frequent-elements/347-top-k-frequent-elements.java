fc9edb459ac553e721261a558c968d979a0caf06
Huu Khang Nguyen <52158153+huukhang1512@users.noreply.github.com>
2022-03-19 23:58:29 +1100

    Time: 20 ms (41.46%), Space: 49.7 MB (63.22%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -4,13 +4,10 @@ class Solution {
         for(int i = 0; i < nums.length; i++){
             map.put(nums[i], map.getOrDefault(nums[i],0) + 1);
         }
-        PriorityQueue<Integer> heap = new PriorityQueue<>((a,b) -> map.get(a) - map.get(b));
+        PriorityQueue<Integer> heap = new PriorityQueue<>((a,b) -> map.get(b) - map.get(a));
         
         for(int num : map.keySet()){
             heap.offer(num);
-            if(heap.size() > k){
-                heap.poll();
-            }
         }
         int[] res = new int[k];
         for(int i = 0 ; i < k; i++){
