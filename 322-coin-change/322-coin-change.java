364ed0b9c9e3dffdcfebd3ef98f50a6a73ae9bac
Huu Khang Nguyen <52158153+huukhang1512@users.noreply.github.com>
2022-03-20 11:58:33 +1100

    Time: 42 ms (29.72%), Space: 44.7 MB (62.98%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,15 +1,26 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int[] dp = new int[amount + 1];
-        Arrays.fill(dp,amount + 1);
+        Arrays.fill(dp,-1);
         dp[0] = 0;
-        for(int i = 1; i <= amount;i++){
-            for(int j = 0; j < coins.length; j++){
-                if(coins[j] <= i){
-                    dp[i] = Math.min(dp[i],1 + dp[i - coins[j]]); // sub problem
-                }
+        int ans = dfs(dp,coins, amount);
+        
+        return ans == Integer.MAX_VALUE ? -1 : ans;
+    }
+    
+    public int dfs(int[] memo, int[] coins, int remain){
+        if(memo[remain] != -1){
+            return memo[remain];
+        }
+        int res = Integer.MAX_VALUE;
+        for(int i = 0; i < coins.length; i++){
+            if(coins[i] <= remain){
+                int ans = dfs(memo, coins, remain - coins[i]);
+                if(ans != Integer.MAX_VALUE)
+                    res = Math.min(res, ans + 1);
             }
         }
-        return dp[amount] == amount + 1 ? -1 : dp[amount];
+        memo[remain] = res;
+        return memo[remain];
     }
 }
