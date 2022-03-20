55b21bf1c794573cdd22698c53c5ed42ed539ff2
Huu Khang Nguyen <52158153+huukhang1512@users.noreply.github.com>
2022-03-20 20:27:41 +1100

    Time: 2 ms (58.97%), Space: 44.8 MB (47.81%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.java
@@ -1,17 +1,18 @@
-
-    class Solution {
-        public int minSubArrayLen(int target, int[] nums) {
-            int res = Integer.MAX_VALUE;
-            int sum = 0;
-            int start = 0 ;// start
-            for(int i = 0; i < nums.length;i++){
-                sum+= nums[i];
-                while (sum >= target){
-                    sum -= nums[start];
-                    res = Math.min(res, i - start +1);
-                    start++;
-                }
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int res = Integer.MAX_VALUE;
+        int start = 0;
+        int curSum = 0;
+        int end = 0;
+        while(end < nums.length){
+            curSum += nums[end];
+            while(curSum >= target){
+                curSum -= nums[start];
+                res = Math.min(res, end - start + 1);
+                start++;
             }
-            return res == Integer.MAX_VALUE ? 0 : res;
+            end++;
         }
+        return res == Integer.MAX_VALUE ? 0 : res;
     }
+}
