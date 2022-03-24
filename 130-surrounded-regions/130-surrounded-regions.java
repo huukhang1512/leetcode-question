a36be223fdbcb075ad39dcb1331b1d71d80196c5
Huu Khang Nguyen <52158153+huukhang1512@users.noreply.github.com>
2022-03-24 11:30:03 +1100

    Time: 1 ms (99.90%), Space: 44.5 MB (85.50%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.java
+++ 130-surrounded-regions/130-surrounded-regions.java
@@ -4,43 +4,51 @@ class Solution {
             return;
         if (board.length < 3 || board[0].length < 3)
             return;
-        int row = board.length;
-        int col = board[0].length;
-        for (int i=0; i<row; i++) {
-            if (board[i][0] == 'O') {
-                dfs(board, i, 0);
+    /*
+        find the area with O only
+        that dont have a cell on the border.
+        
+    */
+        int m = board.length;
+        int n = board[0].length;
+        for(int i = 0; i < m; i++) {
+            if(board[i][0] == 'O'){
+                dfs(i,0, board);
             }
-            if (board[i][col - 1] == 'O') {
-                dfs(board, i, col-1);
-            }
-        }
-        for (int j=0; j<col; j++) {
-            if (board[0][j] == 'O') {
-                dfs(board, 0, j);
-            }
-            if (board[row-1][j] == 'O') {
-                dfs(board, row-1, j);
+            if(board[i][n-1] == 'O'){
+                dfs(i,n-1,board);
             }
         }
-        for (int i=0; i<row; i++) {
-            for (int j=0; j<col; j++) {
-                if (board[i][j] == '+') {
+        for(int i = 0; i < n; i++) {
+            if(board[0][i] == 'O'){
+                dfs(0,i, board);
+            }
+            if(board[m-1][i] == 'O'){
+                dfs(m-1,i,board);
+            }
+        }
+        
+        for(int i = 0; i < m; i++){
+            for(int j = 0 ; j < n; j++){
+                if(board[i][j] == '*'){
                     board[i][j] = 'O';
-                }
-                else if (board[i][j] == 'O') {
+                } else if (board[i][j] == 'O'){
                     board[i][j] = 'X';
                 }
             }
         }
     }
-
-    public void dfs(char [][] board, int i, int j) {
-        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] != 'O')
+    
+    public void dfs(int i,int j, char[][] board){
+        if(i < 0 ||  j < 0 || i > board.length - 1 || j > board[0].length -1 || board[i][j] != 'O'){
             return;
-        board[i][j] = '+';
-        dfs(board, i+1, j);
-        dfs(board, i-1, j);
-        dfs(board, i, j-1);
-        dfs(board, i, j+1);
+        }
+        board[i][j] = '*';
+        
+        dfs(i+1, j, board);
+        dfs(i-1, j, board);
+        dfs(i, j+1, board);
+        dfs(i, j-1, board);
+        
     }
 }
