0245e1ce3dc460dc27893f6c644f96c5c41a7cb3
Huu Khang Nguyen <52158153+huukhang1512@users.noreply.github.com>
2022-03-28 00:18:55 +1100

    Time: 1 ms (87.17%), Space: 45 MB (15.43%) - LeetHub

---
+++ 110-balanced-binary-tree/110-balanced-binary-tree.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if(root == null){
+            return true;
+        }
+        int lDepth = dfs(root.left);
+        int rDepth = dfs(root.right);
+        if(Math.abs(lDepth - rDepth) <= 1){
+            return isBalanced(root.left) && isBalanced(root.right);
+        }
+        return false;
+    }
+    public int dfs(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        return Math.max(1 + dfs(root.right), 1 + dfs(root.left));
+    }
+}
