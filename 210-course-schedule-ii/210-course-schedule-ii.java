13e98d619b22c8951ded5a85a5ff3017dbb34a37
Huu Khang Nguyen <52158153+huukhang1512@users.noreply.github.com>
2022-04-04 13:34:20 +1000

    Time: 192 ms (5.04%), Space: 49.3 MB (56.50%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.java
+++ 210-course-schedule-ii/210-course-schedule-ii.java
@@ -2,10 +2,20 @@ class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         int[] res = new int[numCourses];
         boolean[][] matrix = new boolean[numCourses][numCourses];
+        HashMap<Integer, LinkedList<Integer>> adjList = new HashMap<>();
+        
         int[] inDegree = new int[numCourses];
         for(int i = 0; i < prerequisites.length; i++){
-            inDegree[prerequisites[i][0]]++;
-            matrix[prerequisites[i][1]][prerequisites[i][0]] = true;
+            int pre = prerequisites[i][1];
+            int subject = prerequisites[i][0];
+            inDegree[subject]++;
+            if(adjList.containsKey(pre)){
+                adjList.get(pre).add(subject);
+            } else {            
+                LinkedList<Integer> newList = new LinkedList<>();
+                newList.add(subject);
+                adjList.put(pre, newList);
+            }
         }
         
         LinkedList<Integer> queue = new LinkedList<>();
@@ -19,7 +29,7 @@ class Solution {
             int subject = queue.poll();
             res[index++] = subject;
             for(int i = 0; i < numCourses; i++){
-                if(matrix[subject][i]){
+                if(adjList.containsKey(subject) && adjList.get(subject).contains(i)){
                     inDegree[i]--;
                     if(inDegree[i] == 0){
                         queue.add(i);
