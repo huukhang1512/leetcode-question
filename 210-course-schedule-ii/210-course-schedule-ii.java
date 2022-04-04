ca5240a0d3507f895f354e0c850ba8aa7c771631
Huu Khang Nguyen <52158153+huukhang1512@users.noreply.github.com>
2022-04-04 13:25:16 +1000

    Time: 14 ms (31.58%), Space: 47.9 MB (78.93%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.java
+++ 210-course-schedule-ii/210-course-schedule-ii.java
@@ -1,43 +1,32 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        if(numCourses <= 0) return new int[0];
-        
-        HashMap<Integer, ArrayList<Integer>> preMap = new HashMap<>();
-        HashMap<Integer, Integer> inDegree = new HashMap<>();
-        
-        for(int i = 0 ; i < numCourses; i++){
-            preMap.put(i, new ArrayList<Integer>());
-            inDegree.put(i, 0);
-        }
-        
-        for(int[] prerequisite : prerequisites){
-            int subject = prerequisite[0];
-            int preSubject = prerequisite[1];
-            
-            preMap.get(preSubject).add(subject);
-            inDegree.put(subject, inDegree.get(subject) + 1);
-            
-        }
-        
         int[] res = new int[numCourses];
+        boolean[][] matrix = new boolean[numCourses][numCourses];
+        int[] inDegree = new int[numCourses];
+        for(int i = 0; i < prerequisites.length; i++){
+            inDegree[prerequisites[i][0]]++;
+            matrix[prerequisites[i][1]][prerequisites[i][0]] = true;
+        }
+        
+        LinkedList<Integer> queue = new LinkedList<>();
+        for(int i = 0; i < inDegree.length; i++){
+            if(inDegree[i] == 0){
+                queue.add(i);
+            }
+        }
         int index = 0;
-        while(!inDegree.isEmpty()){
-            boolean isCyclic = true;
-            for(int course: inDegree.keySet()){
-                if(inDegree.get(course) == 0){ // no prerequisite, or 0 indegrees
-                    res[index++] = course;
-
-                    ArrayList<Integer> children = preMap.get(course);
-                    for(int child : children)
-                        inDegree.put(child, inDegree.get(child) - 1);
-                    inDegree.remove(course);
-                    isCyclic = false;
-                    break; 
+        while(!queue.isEmpty()){
+            int subject = queue.poll();
+            res[index++] = subject;
+            for(int i = 0; i < numCourses; i++){
+                if(matrix[subject][i]){
+                    inDegree[i]--;
+                    if(inDegree[i] == 0){
+                        queue.add(i);
+                    }
                 }
             }
-            if (isCyclic)
-                return new int[0]; // empty array
         }
-        return res;
+        return index == numCourses ? res : new int[0];
     }
 }
