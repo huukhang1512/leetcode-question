e4cdd7b628b3f98245c795bea04a1bb384c94263
Huu Khang Nguyen <52158153+huukhang1512@users.noreply.github.com>
2022-04-04 22:42:12 +1000

    Time: 53 ms (8.29%), Space: 46.2 MB (63.19%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,20 +1,20 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        int[][] memo = new int[text1.length() + 1] [text2.length() + 1];
-        return solve(text1, text2, memo,0, 0);
+        int[][] memo = new int[text1.length()][text2.length()];
+        return dfs(text1, text2, 0, 0, memo);
     }
     
-    public int solve(String text1, String text2, int[][] memo, int i, int j){
-        if(i > text1.length() - 1 || j > text2.length() -1){
+    public int dfs(String text1, String text2, int i, int j, int[][] memo){
+        if(i >= text1.length() || j >= text2.length()){
             return 0;
         }
         if(memo[i][j] != 0){
             return memo[i][j];
         }
         if(text1.charAt(i) == text2.charAt(j)){
-            memo[i][j] = 1 + solve(text1, text2, memo, i+1, j+1); // move diaganolly
+            memo[i][j] += 1 + dfs(text1,text2, i+1, j+1, memo);
         } else {
-            memo[i][j] = Math.max(solve(text1, text2, memo, i+1, j),solve(text1, text2, memo, i, j+1)); // move down and right
+            memo[i][j] += Math.max(dfs(text1,text2, i+1, j,memo),dfs(text1,text2, i, j+1,memo));
         }
         return memo[i][j];
     }
