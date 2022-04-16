a8a46174937a52d73d3fbc59ec427f9170ff8791
Huu Khang Nguyen <52158153+huukhang1512@users.noreply.github.com>
2022-04-16 17:43:15 +1000

    Time: 1 ms (83.93%), Space: 43.7 MB (94.03%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.java
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.java
@@ -15,18 +15,18 @@
  */
 class Solution {
     int max = Integer.MIN_VALUE;
+
     public int maxPathSum(TreeNode root) {
-        helper(root);
-        return max;
+        dfs(root);
+        return max;        
     }
-    public int helper(TreeNode cur){
-        if(cur == null){
+    private int dfs(TreeNode root){
+        if(root == null){
             return 0;
         }
-        // recursively through left and right
-        int left = Math.max(helper(cur.left),0); // get the maximum path of the left sub tree
-        int right = Math.max(helper(cur.right),0); // get the maximum path of the right sub tree
-        max = Math.max(max, cur.val + left + right); // result
-        return cur.val + Math.max(left,right); // maximum you could get without split
+        int left = Math.max(dfs(root.left),0);
+        int right = Math.max(dfs(root.right),0);
+        max = Math.max(max, root.val + left + right);
+        return root.val + Math.max(left, right);
     }
 }
