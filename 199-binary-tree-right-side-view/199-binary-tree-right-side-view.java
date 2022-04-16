7a2f2e55e4868728faa5a026e93d7ad330a0044b
Huu Khang Nguyen <52158153+huukhang1512@users.noreply.github.com>
2022-04-16 16:24:41 +1000

    Time: 1 ms (93.36%), Space: 40.9 MB (87.56%) - LeetHub

---
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        return bfs(root);
+        // List<Integer> res = new ArrayList<>();
+        // helper();
+        // return res;
+    }
+    
+    private List<Integer> bfs (TreeNode root){
+        List<Integer> res = new ArrayList<>();
+        if(root == null) return res;
+        
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        
+        queue.add(root);
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            for(int i = 0; i < size; i++){
+                TreeNode cur = queue.poll();
+                
+                if(i == 0) res.add(cur.val);
+                if(cur.right != null){
+                    queue.offer(cur.right);
+                }
+                if(cur.left != null){
+                    queue.offer(cur.left);
+                }
+            }
+        }
+        return res;
+    }
+}
