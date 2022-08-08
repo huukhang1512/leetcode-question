9fca5aed7613d451ffa49bc95213352da8a97685
Huu Khang Nguyen <52158153+huukhang1512@users.noreply.github.com>
2022-08-08 21:39:39 +1000

    Time: 20 ms (5.28%), Space: 42.7 MB (5.06%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,18 +1,15 @@
 class Solution {
-    private Map<String, Integer> map = new HashMap<String, Integer>();
+    public Map<String,Integer> map = new HashMap<>();
     public int uniquePaths(int m, int n) {
-
-        if(m == 1 || n == 1) return 1;
-        
-        String cell = new String(m + "," + n);
-        if(map.containsKey(cell)) 
-            return map.get(cell);
-        
-        int down = uniquePaths(m-1, n);
-        int right = uniquePaths(m, n-1);
-        
-        map.put(cell, down + right);
-        
-        return down + right;
-    }    
+        if(m == 1 || n == 1 ){
+            return 1;
+        }
+        String cell = m + "," + n;
+        if(map.containsKey(cell)) return map.get(cell);
+        int numOfWaysDown = uniquePaths(m-1, n);
+        int numOfWaysRight = uniquePaths(m, n-1);
+        int total = numOfWaysDown + numOfWaysRight;
+        map.put(cell,total);
+        return total;
+    }
 }
